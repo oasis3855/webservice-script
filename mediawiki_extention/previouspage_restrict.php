ef84362650ea6dc3b2291536665a4e71edd4ac96
oasis3855 <oasis3855@users.noreply.github.com>
2012-01-20 11:09:40 +0000

    Ver 1.1 bug fix, for MediaWiki 1.13
    git-svn-id: svn+ssh://oasis.halfmoon.jp/home/tmg1136-inue/var/svn/mediawiki-extention@2 26baa778-a142-e111-a4ce-001d9241b766

--- mediawiki_extention/previouspage_restrict.php
+++ mediawiki_extention/previouspage_restrict.php
@@ -1,41 +1,42 @@
 <?php
+ 
 if (!defined('MEDIAWIKI')) die("MediaWiki extensions cannot be run directly.");
+ 
 /*
 * previouspage_restrict.php    for MediaWiki extention
-* (C) inoue-hiro
-* created 9 Feb 2009
-* tested on MediaWiki 1.13.3
+* (C) INOUE. Hirokazu
 *
 * install : add script on LocalSettings.php
 *   require_once("$IP/extensions/previouspage_restrict.php");
 */
 $wgExtensionCredits['other'][] = array(
     'name' => "previouspage_restrict_extention",
-    'author' => "inoue-hiro",
+    'author' => "INOUE. Hirokazu",
+    'version' => "1.1 (2012/Jan/20) for mw 1.13",
     'description' => "prohibit to open previous pages fo non-logon user",
-    'url' => "http://www.mediawiki.org/",
+    'url' => "http://oasis.halfmoon.jp/mw/index.php?title=Soft-MediaWiki-PreviouspageRestrict-Ext",
 );
  
 $obj_previouspage_restrict = new previouspage_restrict();
-
+ 
 $wgHooks['BeforePageDisplay'][] = array($obj_previouspage_restrict, 'wfMainHookFunction');
-
+ 
 class previouspage_restrict {
-
+ 
     # class constructor
     function previouspage_restrict() {
     }
-
-    function wfMainHookFunction(&$page) {
-
-        global $wgOut, $wgUser, $wgTitle, $wgRequest;       # use global object
  
+    function wfMainHookFunction(&$page) {
+ 
+        global $wgOut, $wgUser, $wgTitle, $wgRequest;       # use global object
+
         ### for debug
-        # echo "<pre>\n";
+        # echo "<!-- <pre>\n";
         # print_r($wgUser);
         # print_r($wgTitle);
         # print_r($wgRequest);
-        # echo "\n</pre>\n";
+        # echo "\n</pre> -->\n";
         ### for debug
  
         # if Loggedin, do nothing (return)
@@ -43,11 +44,8 @@ class previouspage_restrict {
             # isLoggedin() is defined includes/User.php
             return true;
         }
-
+ 
         # if not defined oldid(previous page) and not history mode, do nothing (return)
-#       if(empty($wgRequest->data['oldid']) && $wgRequest->data['action'] != 'history') {
-#           return true;
-#       }
         if(empty($wgRequest->data['oldid'])) {
             if(empty($wgRequest->data['action'])) {
                 return true;
@@ -56,17 +54,15 @@ class previouspage_restrict {
                 return true;
             }
         }
-        
+ 
         # if prohibited previous page, display error message insted of Wiki article
         # show error screen, message is defined at languages/messages/MessagesXX.php
         $wgOut->showErrorPage( 'notloggedin', 'prefsnologintext' );
         # set contentSub
         $wgOut->setSubtitle( 'topic history is not available' );
-    
-    
+ 
         return true;
-        
-    } # function wfMainHookFunction
-} # class previouspage_restrict
 
-## eof
+    } # function wfMainHookFunction
+ 
+} # class previouspage_restrict
--- mediawiki_extention/specialpage_restrict.php
+++ mediawiki_extention/specialpage_restrict.php
@@ -1,76 +1,79 @@
 <?php
+ 
 if (!defined('MEDIAWIKI')) die("MediaWiki extensions cannot be run directly.");
+ 
 /*
 * specialpage_restrict.php    for MediaWiki extention
-* (C) inoue-hiro
-* created 9 Feb 2009
-* tested on MediaWiki 1.13.3
+* (C) INOUE. Hirokazu
 *
 * install : add script on LocalSettings.php
 *   require_once("$IP/extensions/specialpage_restrict.php");
 */
 $wgExtensionCredits['other'][] = array(
     'name' => "specialpage_restrict_extention",
-    'author' => "inoue-hiro",
-    'description' => "prohibit to open special/user pages fo non-logon user",
-    'url' => "http://www.mediawiki.org/",
+    'author' => "INOUE. Hirokazu",
+    'version' => "1.1 (2012/Jan/20) for mw 1.13",
+    'description' => "prohibit to open special pages fo non-logon user",
+    'url' => "http://oasis.halfmoon.jp/mw/index.php?title=Soft-MediaWiki-SpecialpageRestrict-Ext",
 );
  
+ 
 $obj_specialpage_restrict = new specialpage_restrict();
-
+ 
+# MediaWiki Hook (MediaWikiのフック機能)
 $wgHooks['BeforePageDisplay'][] = array($obj_specialpage_restrict, 'wfMainHookFunction');
-
+ 
+ 
 class specialpage_restrict {
-
+ 
     # class constructor
     function specialpage_restrict() {
     }
-
-    function wfMainHookFunction(&$page) {
-
-        global $wgOut, $wgUser, $wgTitle;       # use global object
  
+    function wfMainHookFunction(&$page) {
+        global $wgOut, $wgUser, $wgTitle;       # use global object
+
         ### for debug
-        # echo "<pre>\n";
+        # echo "<!-- <pre>\n";
         # print_r($wgUser);
         # print_r($wgTitle);
-        # echo "\n</pre>\n";
+        # echo "\n</pre> -->\n";
         ### for debug
  
-        # if Loggedin, do nothing (return)
+        # if Loggedin, do nothing (return) (ログイン済みの時は何もしない)
         if($wgUser->isLoggedIn()) {
             # isLoggedin() is defined includes/User.php
             return true;
         }
-
-        # if not Special:, do nothing (return)
+ 
+        # if not Special:, do nothing (return) (Special:ページとUser:ページ以外では何もしない)
         if($wgTitle->mNamespace != NS_SPECIAL && $wgTitle->mNamespace != NS_USER) {
-            # NS_SPECIAL is defined at includes/Defines.php
+            # NS_SPECIAL, NS_USER is defined at includes/Defines.php
             return true;
         }
-
+ 
         $bAllowed = false;  # this is set true, if matched to AllowTitles
-        
+
+        # Special:ページのうち、ログイン、ログアウトページのみは許可する
         $arrAllowTitle = array(SpecialPage::getTitleFor( 'Userlogin' ), SpecialPage::getTitleFor( 'Userlogout' ));  # array of AllowedTitles
-        
-        # check Allowed Titles
+        # check Allowed Titles (許可されたページかどうか判別する)
         foreach($arrAllowTitle as $sAllowTitle) {
-#           if($wgTitle->mTextform == $sAllowTitle) {   # mw 1.13
+#            if($wgTitle->mTextform == $sAllowTitle) {      # before MediaWiki ver 1.13
             if($wgTitle->mPrefixedText == $sAllowTitle) {
                 # Allowed Title
                 $bAllowed = true;
             }
         }
-        
+ 
         # if prohibited Special: page, display error message insted of Wiki article
+        # (制限ページに合致した場合、エラーメッセージを表示する)
         if($bAllowed == false) {
             # show error screen, message is defined at languages/messages/MessagesXX.php
             $wgOut->showErrorPage( 'notloggedin', 'prefsnologintext' );
         }
-
+ 
         return true;
-        
-    } # function wfRestrictSpecialPage
-} # class obj_ipuser_restrict
 
-## eof
+    } # function wfRestrictSpecialPage
+ 
+} # class obj_ipuser_restrict
