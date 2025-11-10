4bc37909a2ef610357198938b807d6c502ca81b6
inoue.h <oasis3855@users.noreply.github.com>
2025-11-10 12:26:36 +0900

    web-file-browser

--- web-file-browser/config.php
+++ web-file-browser/config.php
@@ -1,9 +1,10 @@
 <?php
 
-$strBaseDir = $info['dir'].'/www';
+$USER_HOME_DIR = posix_getpwuid(posix_geteuid())['dir'];
+$strBaseDir = $USER_HOME_DIR.'/www';
 
 // HTTP絶対パス（最後のスラッシュは含まない）
-$strAbsolutePath = 'http://www.example.com';
+$strAbsolutePath = 'https://www.example.com';
 
 // 画像幅指定
 $nFixWidth = 320;
